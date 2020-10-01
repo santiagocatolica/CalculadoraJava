a3202d6489dcec4b2dc2227b8982e0c8480dda15
santiagocatolica <santiago.negreira@catolicasc.org.br>
2020-10-01 10:38:47 -0300

    Inserindo testes para a Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -7,6 +7,35 @@ public class Calculadora {
 		Scanner in = new Scanner(System.in);
 		double num1, num2, soma, subtracao, multi, divi, op;
 		
+	// ------------------------------ TESTE -------------------------------------
+		// Método que será executado antes dos testes
+		@Before
+		public void initTest() {
+			// Instância do objeto
+			calculadora = new Calculadora();
+			// Valor Mockado
+			  // Valor1
+				num1 = 10;
+			  // Valor2
+				num2 = 2;
+		};
+		
+		// Método de soma testado de forma unitária.
+		@Test
+		public void testAddittion() {
+			// chamada do método de adição do meu objeto calculadora com valores mockados sendo passados como parâmetro.
+			soma = calculadora.addittion(num1, num2);
+			// verificação dos valores esperados com o valor retornado, arredondado
+			assertEquals(12, result, 0.001);
+		}
+		
+		
+		// Método que será executado após os testes unitários
+		@After
+		public void afterTest() {
+			calculadora = null;
+		}
+		
 		// Escolhendo a operação
 		System.out.println("Escolha uma operacao: ");
 		System.out.println("1 - Soma, 2 - Subtracao, 3 - Multiplicacao, 4 - Divisao");
